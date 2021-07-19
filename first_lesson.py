07d1b2ab9d7f64436e715105edceacb41477f1ef
Anastasiya Krach <sunnyyannik49@yandex.by>
2021-07-19 20:41:01 +0300

    initial commit

---
+++ first_lesson.py
@@ -0,0 +1,23 @@
+if False:
+    print('hi')
+else:
+    print('bye')
+__093 = 5
+print(__093)
+print(help("keywords"))
+new_string='''
+adsfdfg
+asffhijp
+seryr6t
+'''
+print(new_string)
+s = '1234'
+x = int(s)
+print(x)
+print(type(s))
+s = 3.6
+x = int(s)
+print(x)
+print(type(s))
+
+
---
+++ lesson2.py
@@ -0,0 +1,7 @@
+s = 'abbadfffglkj'
+print(s.find('bba'))
+s = 'abbadfffglkj'
+s = s.replace('abba','new').replace('glkj','now')
+print (s)
+s =":"
+print(s.join(["1345", "fsdfdg", "4"]))
